3f3f22f16e474a86f02846682d7cd452dc8931b3
rodrigo <rherdia609@g.educaand.es>
2025-05-04 15:39:37 +0200

    calculadora solo suma

---
+++ calculadoraEntornos/src/calculadoraentornos/Calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadoraentornos;
+
+/**
+ *
+ * @author xxrod
+ */
+public class Calculadora {
+    private int resultado;
+    
+
+    public Calculadora() {
+    }
+
+    public int suma(int num1, int num2){
+        resultado = num1 + num2;
+        return resultado;
+    }
+    
+    
+}
